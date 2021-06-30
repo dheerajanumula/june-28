6b45ab18e32b5455debca5c26858f2ce5d9f4f40
dheerajanumula <86674086+dheerajanumula@users.noreply.github.com>
2021-06-30 11:15:53 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,16 @@
+
+a='30'
+b='30.5'
+c='dheeraj'
+d=False
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
+print(d,type(d))
+
+a=int(a)
+b=float(b)
+c=str(d)
+print(a,type(a))
+print(b,type(b))
+print(c,type(c))
