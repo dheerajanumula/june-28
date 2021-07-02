2b435a357d3363bef8b518284d9e0ecc0267a7a0
dheerajanumula <86674086+dheerajanumula@users.noreply.github.com>
2021-07-02 09:32:14 +0530

    Add files via upload

---
+++ operaters.py
@@ -0,0 +1,13 @@
+#operators
+a=int(input("Enter the first number a:\n"))
+b=int(input("Enter the second number b:\n"))
+print("addition of {} and {} is {}".format(a,b,a+b))
+print("subraction of {} and {} is {}".format(a,b,a-b))
+print("mutliplication of {} and {} is {}".format(a,b,a*b))
+print("division of {} and {} is {}".format(a,b,a/b))
+print("ground division of {} and {} is {}".format(a,b,a//b))
+print("modulo of {} and {} is {}".format(a,b,a%b))
+print(" {}power{} is {}".format(a,b,a**b))
+print("a is less than b true or false:\n{}".format(a<b))
+print("a is greater than b true or false:\n{}".format(a>b))
+print("a is equal to b true or false:\n{}".format(a==b))
